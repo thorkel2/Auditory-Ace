2ca1a5960df5d0aea69b8e7cf2bc98247d0b1ab1
Jordan <jhn100550@gmail.com>
2024-01-19 19:01:56 -0800

    Set up volumes for TTS + Background Noise
    Framework for volumes for TTS and BG noise set up

--- App-Files/Scripts/TextToSpeech.gd
+++ App-Files/Scripts/TextToSpeech.gd
@@ -1,14 +1,22 @@
-extends Node
 #This script contains the function for playing text via Text-to-Speech
 #To play a string, create a var that contains the TextToSpeech class: "@onready var TTS = TextToSpeech"
 #Then, simply call the "playText" function to play string: TTS.playText("Insert text here")
+extends Node
 
 class_name TTS
+#Contains the voices that are usable
+var voices = DisplayServer.tts_get_voices() 
 
-var voices = DisplayServer.tts_get_voices()
-var voice = voices[0]
+#Used to determine which voice to use when playing TTS
+@onready var profile = Profile 
 
-func playText(text): 
-	var child = voice["name"]
-	DisplayServer.tts_speak(text, child)
+#Will play text inputted via TTS
+func playText(text : String): 
+	DisplayServer.tts_speak(text, voices[profile.Voice]["id"], profile.Volume)
 	
+#Returns the names of all voices in system
+func getVoices(): 
+	var Voices: Array[String]
+	for i in voices:
+		Voices.append(i["name"])
+	return Voices
