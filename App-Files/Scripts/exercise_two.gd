78ca63afc142efd8fea54322fd99207556a83115
lilaclayla <117493676+lilaclayla@users.noreply.github.com>
2024-02-13 08:56:51 -0800

    Cleaning Up Code!
    consolidated button scripts to scene scripts, deleted unused scripts

--- App-Files/Scripts/exercise_two.gd
+++ App-Files/Scripts/exercise_two.gd
@@ -1,8 +1,16 @@
-extends Button
+extends Node2D
+
+func _on_cancel_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
 
 
-#func load_next_scene():
-#	LoadingScreen.load_scene("res://Scenes/ExerciseTwo")
+func _on_profile_pressed():
+	get_tree().change_scene_to_file("res://Scenes/profile.tscn")
 
-func _on_pressed():
-	get_tree().change_scene_to_file("res://Scenes/pre_exercise_two_screen.tscn")
+
+func _on_help_pressed():
+	get_tree().change_scene_to_file("res://Scenes/help.tscn")
+	
+
+func _on_home_pressed():
+	get_tree().change_scene_to_file("res://Scenes/home.tscn")
--- App-Files/post_exercise_screen.gd
+++ App-Files/post_exercise_screen.gd
@@ -1,12 +1,20 @@
 extends Node2D
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+func _on_cancel_pressed():
+	get_tree().change_scene_to_file("res://Scenes/pre_exercise_one_screen.tscn")
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	
-	pass
+func _on_done_pressed():
+	get_tree().change_scene_to_file("res://Scenes/main_menu.tscn")
+
+
+func _on_profile_pressed():
+	get_tree().change_scene_to_file("res://Scenes/profile.tscn")
+
+
+func _on_help_pressed():
+	get_tree().change_scene_to_file("res://Scenes/help.tscn")
+
+
+func _on_home_pressed():
+	get_tree().change_scene_to_file("res://Scenes/home.tscn")
