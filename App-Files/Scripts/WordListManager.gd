e3cca256a673df3c210a4408170347129ed9c48c
Darya Haines <92010373+MissMoreau@users.noreply.github.com>
2024-02-19 15:40:43 -0800

    word list updates

--- App-Files/Scripts/WordListManager.gd
+++ App-Files/Scripts/WordListManager.gd
@@ -35,9 +35,9 @@ var usedtVpWords := []
 # Initialization function called when the Node enters the scene tree
 func _ready():
 	# Load the word sets from the CSV files
-	loadWordSets("res://m vs n.csv", WordListType.MVN) # M vs N word types
-	loadWordSets("res://s vs f.csv", WordListType.SVF) # S vs F word types
-	loadWordSets("res://t vs p.csv", WordListType.TVP) # T vs P word types
+	loadWordSets("res://App-Files/Word-Lists/m vs n.csv", WordListType.MVN) # M vs N word types
+	loadWordSets("res://App-Files/Word-Lists/s vs f.csv", WordListType.SVF) # S vs F word types
+	loadWordSets("res://App-Files/Word-Lists/t vs p.csv", WordListType.TVP) # T vs P word types
 
 # Load the word sets from a CSV file and add them to the appropriate list
 func loadWordSets(filePath: String, type: WordListType) -> void:
@@ -92,9 +92,6 @@ func getRandomWordSet(type: WordListType, desiredSound: String) -> WordSet:
 	if wordSets.size() == 0:
 		print("Error: Current word list is empty.")
 		return null
-	if wordSets.size() == 0:
-		print("Error: Current word list is empty.")
-		return null
 	
 	# Filter word sets based on the desired sound type and exclude used words
 	var filteredWordSets := []
@@ -104,17 +101,10 @@ func getRandomWordSet(type: WordListType, desiredSound: String) -> WordSet:
 	
 	if filteredWordSets.size() == 0:
 		usedWords = []
-	if filteredWordSets.size() == 0:
-		usedWords = []
 	
 	# Get a random word set from the filtered list
 	var randomIndex := randi() % filteredWordSets.size()
 	var selectedWordSet := filteredWordSets[randomIndex]
-	# Get a random word set from the filtered list
-	var randomIndex := randi() % filteredWordSets.size()
-	var selectedWordSet := filteredWordSets[randomIndex]
 	
-	usedWords.append(selectedWordSet.correctWord)
-	return selectedWordSet
 	usedWords.append(selectedWordSet.correctWord)
 	return selectedWordSet
