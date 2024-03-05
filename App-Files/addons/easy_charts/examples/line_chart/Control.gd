48d1358705ef45ebbc9aab9cb516271e02816b04
lilaclayla <117493676+lilaclayla@users.noreply.github.com>
2024-03-04 22:18:50 -0800

    Fixing Stats page!

--- App-Files/addons/easy_charts/examples/line_chart/Control.gd
+++ App-Files/addons/easy_charts/examples/line_chart/Control.gd
@@ -1,18 +1,24 @@
 extends Control
 
 @onready var chart: Chart = $VBoxContainer/Chart
-@onready var entries = Database.searchEntries("All","All","All","All")
+
+var entries = Database.searchEntries("All","All","All","All")
+var date = entries[0];
+var score = entries[1];
+var result_array = []
 
 # This Chart will plot 3 different functions
 var f1: Function
 
+func create_array(n: int) -> Array:
+	var result_array = []
+	for i in range(n):
+		result_array.append(i)
+	return result_array
+
 func _ready():
-	# Let's create our @x values
-	var x: Array = entries[1];
-	
-	# And our y values. It can be an n-size array of arrays.
-	# NOTE: `x.size() == y.size()` or `x.size() == y[n].size()`
-	var y: Array = entries[0];
+	var x: Array = create_array(date.size())
+	var y: Array = score
 	
 	# Let's customize the chart properties, which specify how the chart
 	# should look, plus some additional elements like labels, the scale, etc...
@@ -24,22 +30,22 @@ func _ready():
 	cp.colors.text = Color("#000000")
 	cp.draw_bounding_box = false
 	cp.x_label = "Time"
-	cp.y_label = "Accuracy"
-	cp.x_scale = 1
-	cp.y_scale = 1
+	cp.y_label = "Score"
+	cp.x_scale = 10
+	cp.y_scale = 10
 	cp.interactive = true # false by default, it allows the chart to create a tooltip to show point values
 	# and interecept clicks on the plot
 	
 	# Let's add values to our functions
 	f1 = Function.new(
-		x, y, "Pressure", # This will create a function with x and y values taken by the Arrays 
+		x, y, "Score Over Time", # This will create a function with x and y values taken by the Arrays 
 						# we have created previously. This function will also be named "Pressure"
 						# as it contains 'pressure' values.
 						# If set, the name of a function will be used both in the Legend
 						# (if enabled thourgh ChartProperties) and on the Tooltip (if enabled).
 		# Let's also provide a dictionary of configuration parameters for this specific function.
 		{ 
-			color = Color("#36a2eb"), 		# The color associated to this function
+			color = Color("#FFFFFF"), 		# The color associated to this function
 			marker = Function.Marker.CIRCLE, 	# The marker that will be displayed for each drawn point (x,y)
 											# since it is `NONE`, no marker will be shown.
 			type = Function.Type.LINE, 		# This defines what kind of plotting will be used, 
@@ -51,6 +57,3 @@ func _ready():
 	
 	# Now let's plot our data
 	chart.plot([f1], cp)
-	
-	# Uncommenting this line will show how real time data plotting works
-	set_process(false)
