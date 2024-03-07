6832e284fdb091bb12358b8f130018d4b0408bb9
lilaclayla <117493676+lilaclayla@users.noreply.github.com>
2024-03-07 09:09:04 -0800

    UI Finishing Touches

--- App-Files/addons/easy_charts/examples/line_chart/Control.gd
+++ App-Files/addons/easy_charts/examples/line_chart/Control.gd
@@ -33,6 +33,7 @@ func _ready():
 	cp.y_label = "Score"
 	cp.x_scale = 10
 	cp.y_scale = 5
+	#cp.x_tick_size = 60
 	cp.interactive = true # false by default, it allows the chart to create a tooltip to show point values
 	# and interecept clicks on the plot
 	
@@ -45,7 +46,7 @@ func _ready():
 						# (if enabled thourgh ChartProperties) and on the Tooltip (if enabled).
 		# Let's also provide a dictionary of configuration parameters for this specific function.
 		{ 
-			color = Color("#FFFFFF"), 		# The color associated to this function
+			color = Color("#004999"), 		# The color associated to this function
 			marker = Function.Marker.CIRCLE, 	# The marker that will be displayed for each drawn point (x,y)
 											# since it is `NONE`, no marker will be shown.
 			type = Function.Type.LINE, 		# This defines what kind of plotting will be used, 
