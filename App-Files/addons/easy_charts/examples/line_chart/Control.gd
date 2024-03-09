a0b6eb32397b4cbb8d084f2c891a76c7f87df365
lilaclayla <117493676+lilaclayla@users.noreply.github.com>
2024-03-09 04:52:22 -0800

    Stats working! Adding wordlist filter (#70)
    * small adjustments
    
    * UI Finishing Touches
    
    * changed capitalization
    
    * chart changes
    
    * small adjustments
    
    * UI Finishing Touches
    
    * stash changes
    
    * fixed chart, working on search by word list
    
    * fix capital
    
    * Added wordlist filter
    
    * Just worked magic idk

--- App-Files/addons/easy_charts/examples/line_chart/Control.gd
+++ App-Files/addons/easy_charts/examples/line_chart/Control.gd
@@ -1,36 +1,38 @@
 extends Control
 
 @onready var chart: Chart = $VBoxContainer/Chart
+@onready var wordlist = Globals.wordlist if Globals.wordlist else "All"
+@onready var bgnoises = Globals.bgnoises if Globals.bgnoises else "All"
+@onready var exercise = Globals.exercise if Globals.exercise else "All"
+@onready var entries = Database.searchEntries("All",bgnoises,wordlist,exercise)
+@onready var score = [0];
+@onready var date = [0];
+@onready var result_array = [];
 
-var entries = Database.searchEntries("All","All","All","All")
-var date = entries[0] if entries else [0];
-var score = entries[1] if entries else [0];
-var result_array = []
-
-# This Chart will plot 3 different functions
+var cp: ChartProperties = ChartProperties.new()
 var f1: Function
-
 func create_array(n: int) -> Array:
 	var result_array = []
-	for i in range(n):
+	for i in range(1, n + 1):
 		result_array.append(i)
 	return result_array
 
 func _ready():
-	var x: Array = create_array(date.size())
+	if(entries[0]):
+		var temp = create_array(entries[0].size())
+		date.append_array(temp)
+	if(entries[1]):
+		score.append_array(entries[1])
+	var x: Array = date
 	var y: Array = score
-	
 	# Let's customize the chart properties, which specify how the chart
 	# should look, plus some additional elements like labels, the scale, etc...
-	var cp: ChartProperties = ChartProperties.new()
 	cp.colors.frame = Color("#daedfa")
 	cp.colors.background = Color("daedfa")
 	cp.colors.grid = Color("#283442")
 	cp.colors.ticks = Color("#283442")
 	cp.colors.text = Color("#000000")
 	cp.draw_bounding_box = false
-	cp.x_label = "Time"
-	cp.y_label = "Score"
 	cp.x_scale = 10
 	cp.y_scale = 5
 	#cp.x_tick_size = 60
@@ -57,4 +59,40 @@ func _ready():
 	)
 	
 	# Now let's plot our data
+	print(entries)
+	print(x)
+	print(y)
 	chart.plot([f1], cp)
+
+func _on_word_list_dropdown_item_selected(index):
+	if(index == 0):
+		Globals.wordlist = "All"
+	if(index == 1):
+		Globals.wordlist = "MvN"
+	if(index == 2):
+		Globals.wordlist = "SvF"
+	if(index == 3):
+		Globals.wordlist = "TvP"
+	get_tree().change_scene_to_file("res://Scenes/profile.tscn")
+	
+func _on_bg_noise_dropdown_item_selected(index):
+	if(index == 0):
+		Globals.bgnoises = "All"
+	if(index == 1):
+		Globals.bgnoises = "Low"
+	if(index == 2):
+		Globals.bgnoises = "Medium"
+	if(index == 3):
+		Globals.bgnoises = "High"
+	if(index == 4):
+		Globals.bgnoises = "None"
+	get_tree().change_scene_to_file("res://Scenes/profile.tscn")
+
+func _on_exercise_item_selected(index):
+	if(index == 0):
+		Globals.exercise = "All"
+	if(index == 1):
+		Globals.exercise = "1"
+	if(index == 2):
+		Globals.exercise = "2"
+	get_tree().change_scene_to_file("res://Scenes/profile.tscn")
