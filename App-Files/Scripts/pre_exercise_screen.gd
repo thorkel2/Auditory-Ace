6dae62ad3381db59c118f353f2c911439b7ae30d
dushuynh <dushuynh@pdx.edu>
2024-03-11 15:10:21 -0700

    Pre-exercise BG Noise changes

--- App-Files/Scripts/pre_exercise_screen.gd
+++ App-Files/Scripts/pre_exercise_screen.gd
@@ -17,14 +17,12 @@ func _ready():
 	# Choosing random word list in case user doesn't pick
 	WordListManager.setWordListVar(randi() % 3 + 1)
 	
-	# Default BG volume to low
+	# Default lastVolumePressed to low
 	lastVolumePressed = BGLow
-	Audio.changeBGVolume("BG", "Low")
 	
-	# Disable BG options
-	BGLow.set_disabled(true)
-	BGMedium.set_disabled(true)
-	BGHigh.set_disabled(true)
+	# Default bgLevel to None
+	_on_bg_noise_dropdown_item_selected(0)
+	
 	
 	if(Globals.backscene == "res://Scenes/exercise_one.tscn"):
 		descriptionText.text = "Let’s practice 
@@ -108,24 +106,12 @@ func _on_bg_noise_dropdown_item_selected(index):
 # The four BG buttons "turn off" the other BG buttons when one is pressed
 func _on_bg_low_pressed():
 	BGButtonsLogic(BGLow)
-	TextToSpeech.playText("Low")
-	WordListManager.bgLevel = "Low"
-	Audio.changeBGVolume("BG", "Low")
-	Audio.playBGNoise()
 
 func _on_bg_medium_pressed():
 	BGButtonsLogic(BGMedium)
-	TextToSpeech.playText("Medium")
-	WordListManager.bgLevel = "Medium"
-	Audio.changeBGVolume("BG", "Medium")
-	Audio.playBGNoise()
 
 func _on_bg_high_pressed():
 	BGButtonsLogic(BGHigh)
-	TextToSpeech.playText("High")
-	WordListManager.bgLevel = "High"
-	Audio.changeBGVolume("BG", "High")
-	Audio.playBGNoise()
 
 # Background volume button on/off logic
 func BGButtonsLogic(buttonPressed):
@@ -137,3 +123,19 @@ func BGButtonsLogic(buttonPressed):
 			button.set_texture_normal(onButton)
 		else:
 			button.set_texture_normal(offButton)
+	
+	match buttonPressed:
+		BGLow:
+			TextToSpeech.playText("Low")
+			WordListManager.bgLevel = "Low"
+			Audio.changeBGVolume("BG", "Low")
+		BGMedium:
+			TextToSpeech.playText("Medium")
+			WordListManager.bgLevel = "Medium"
+			Audio.changeBGVolume("BG", "Medium")
+		BGHigh:
+			TextToSpeech.playText("High")
+			WordListManager.bgLevel = "High"
+			Audio.changeBGVolume("BG", "High")
+	
+	Audio.playBGNoise()
